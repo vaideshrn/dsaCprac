2789aa36fe2d498fd7b5350becd73242ae5bd7ee
Vaidesh <vaidesh.raja38@gmail.com>
2025-05-03 13:55:24 +0530

    .

--- search_in_a_matrix.c
+++ search_in_a_matrix.c
@@ -1,3 +1,16 @@
-//
-// Created by Vaide on 03/05/2025.
-//
+#include<stdio.h>
+int main() {
+    int mat[3][3] = {{6,23,21},{4,45,32},{69,11,87}};
+    int x = 32;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(mat[i][j] == x) {
+                printf("%d is present in the matrix",x);
+                break;
+            }
+            else {
+                continue;
+            }
+        }
+    }
+}
